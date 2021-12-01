3750b84997c3b91e6cc566a95baa5e6cbb0c1a48
Pengxiang Zhang <z-git@protonmail.com>
2021-12-01 14:35:21 -0600

    Class meeting

---
+++ Assignment/Final Project/Assets/Access/Player/TextChanger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    public TextMeshProUGUI m_TextComponent;
+    // Start is called before the first frame update
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name == "City") 
+            m_TextComponent.text = "Welcome, You are in a city.\nYou are trying to get to a christmas party.";
+        else if (other.gameObject.name == "Suburban")
+            m_TextComponent.text = "Welcome, You are in a suburban.\nYou are trying to get to a christmas party.";
+        else if (other.gameObject.name == "Forest")
+            m_TextComponent.text = "Welcome, You are in a Forest.\nYou are trying to get to a christmas party.";
+        else if (other.gameObject.name == "Construction")
+            m_TextComponent.text = "Welcome, You are in a Construction Site.\nYou are trying to get to a christmas party.";
+        else if (other.gameObject.name == "Snow")
+            m_TextComponent.text = "Welcome to the christmas party.\nHave a great holiday!";
+    }
+}
